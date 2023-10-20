5d765b3eb81e5483c5f2c3131f88d7651d8cf369
K <redknclarke@gmail.com>
2023-10-20 08:50:52 -0400

    added comments and TODOs

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,7 +1,5 @@
 #include "main.h"
 
-extern bool pto_endgame_enabled;
-extern float pto_cooldown;
 extern pros::Motor& PTO_left;
 extern pros::Motor& PTO_right;
 extern pros::ADIDigitalOut PTO_piston;
@@ -14,15 +12,36 @@ const int INTAKE_SPEED = 127;
 const int CATAPULT_SPEED = 127;
 const int RELATIVE_CHARGE_DIST = 720;
 const int RELATIVE_SHOOT_DIST = 50;
+
+bool pto_endgame_enabled = false;
+float pto_cooldown = 0;
+bool intake_toggle_enabled = false;
+
 void rumble_controller() {
-  master.rumble("....");  //⁡⁢⁣⁢THIS USES MORSE CODE!!!!⁡
+  master.rumble("....");
 }
 
 void print_stats_controller() {
+  // Clear the controller screen
+  master.clear();
+
+  // Print PTO mode
   if (pto_endgame_enabled) {
     master.set_text(0, 0, "4 motor");
   } else {
     master.set_text(0, 0, "6 motor");
+  }
+}
+
+void toggle_endgame(bool toggle){
+  // Only use endgame if PTO is in 4 motor mode
+  if(!pto_endgame_enabled) return;
+
+  // Set the endgame motors to the correct position
+  if(toggle){
+    // TODO turn on the endgame
+  } else{
+    // TODO turn off the endgame
   }
 }
 
@@ -53,31 +72,44 @@ void set_intake_volts(int volts) {
 }
 
 void pto_toggle(bool toggle) {
-  //This prevents extreme air loss using a cooldown
+  // This prevents extreme air loss using a cooldown
   if (pto_cooldown > 0) {
     return;
   }
+  // Set the PTO cooldown
   pto_cooldown = ez::util::DELAY_TIME * 50;
+  
+  // Toggle PTO motors + bool
   pto_endgame_enabled = toggle;
   chassis.pto_toggle({PTO_left, PTO_right}, toggle);
 
-  //⁡⁢⁢⁣TODO ⁡make sure this line isnt the issue in the chassis
+  // Actuate the piston 
   PTO_piston.set_value(!toggle);
-  if (toggle) {
-    PTO_left.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    PTO_right.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-  }
 }
 
 void set_pto_volts(int volts) {
+  // Only activates if engame is enabled
   if (!pto_endgame_enabled)
     return;
-  // Sets intake voltage to the input value
+  
+  // Sets endgame voltage to the input value
   PTO_left = volts;
   PTO_right = volts;
 }
 
 void intake_control() {
+  // Toggle the intake (inward direction only)
+  if (master.get_digital_new_press(DIGITAL_R1) || master.get_digital_new_press(DIGITAL_R2)) {
+    intake_toggle_enabled = !intake_toggle_enabled;
+  }
+
+  // If toggled, intake stays on
+  if (intake_toggle_enabled) {
+    set_intake_volts(12000);
+    return;
+  }
+
+  // Hold buttons to control the intake (while not toggled)
   if (master.get_digital(DIGITAL_L1)) {
     set_intake_volts(12000);
   } else if (master.get_digital(DIGITAL_L2)) {
@@ -88,19 +120,11 @@ void intake_control() {
 }
 
 void pto_control() {
-  // Handle PTO activation /deactivation
+  // Handle PTO activation/deactivation
   if (master.get_digital(DIGITAL_A))
     pto_toggle(!pto_endgame_enabled);
   else if (master.get_digital(DIGITAL_DOWN))
     pto_toggle(0);
   else if (master.get_digital(DIGITAL_UP))
     pto_toggle(1);
-  // Move with buttons if PTO set to endgame :3
-  // Likely to be replaced with a different control scheme
-  if (master.get_digital(DIGITAL_L1))
-    set_pto_volts(127);
-  else if (master.get_digital(DIGITAL_L2))
-    set_pto_volts(-127);
-  else
-    set_pto_volts(0);
 }

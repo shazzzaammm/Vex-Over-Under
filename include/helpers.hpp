9cbb9554cf1b81ced27361e077322be2ca400323
K <redknclarke@gmail.com>
2023-10-23 10:24:48 -0400

    altered some functions

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -3,15 +3,16 @@ void print_stats_controller();
 
 void charge_catapult();
 void shoot_catapult();
-void move_catapult(int degrees);
+void move_catapult(float degrees);
 
 void set_intake_volts(int volts);
-void spin_intake_for(float dist);
+void spin_intake_for(float degrees);
 void intake_control();
 
 void pto_toggle(bool toggle);
 void set_pto_volts(int volts);
 void pto_control();
+int get_pto_mode();
 
 void wing_toggle(bool toggle);
 void wing_control();

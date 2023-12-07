ff34bc2014c1c65687ac6f2b0c0461c1c2051724
K <Redknclarke@gmail.com>
2023-12-07 07:30:15 -0500

    updated controls

--- include/variables.hpp
+++ include/variables.hpp
@@ -9,7 +9,6 @@ bool intake_toggle_enabled = false;
 bool outtake_toggle_enabled = false;
 
 // Catapult
-const int CATAPULT_CHARGING_VOLTAGE = 12000;
 const int CATAPULT_SHOOTING_VOLTAGE = 12000;
 bool catapult_auto_shoot_enabled = false;
 
@@ -27,14 +26,14 @@ bool wings_enabled = false;
 bool chassisIsReversed = false;
 
 // Controls
-const ControlScheme t_controls(DIGITAL_R2, DIGITAL_L2, DIGITAL_R1, DIGITAL_L1, DIGITAL_B, DIGITAL_A, DIGITAL_Y,
-                               DIGITAL_X, DIGITAL_DOWN);
+const ControlScheme t_controls(DIGITAL_R2, DIGITAL_L2, DIGITAL_R1, DIGITAL_L1, DIGITAL_Y, DIGITAL_DOWN, DIGITAL_B,
+                               DIGITAL_A, DIGITAL_X);
 const ControlScheme g_controls(DIGITAL_L1, DIGITAL_L2, DIGITAL_R1, DIGITAL_R2, DIGITAL_A, DIGITAL_B, DIGITAL_X,
                                DIGITAL_Y, DIGITAL_DOWN);
 const ControlScheme k_controls(DIGITAL_L2, DIGITAL_L1, DIGITAL_R2, DIGITAL_R1, DIGITAL_B, DIGITAL_A, DIGITAL_Y,
                                DIGITAL_X, DIGITAL_DOWN);
-ControlScheme selected_controls = k_controls;
+ControlScheme selected_controls = g_controls;
 
 // Util
-const int MATCH_LENGTH = 105000;
+const int MATCH_LENGTH = 105000; // = 105 seconds
 int match_start_time = 0;

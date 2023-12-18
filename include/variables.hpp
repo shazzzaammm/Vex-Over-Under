81696425a381ac64717004c6f7fac7f19365cc0d
K <Redknclarke@gmail.com>
2023-12-18 09:36:03 -0500

    updated controls and tuned the optic sensor

--- include/variables.hpp
+++ include/variables.hpp
@@ -13,7 +13,7 @@ const int CATAPULT_SHOOTING_VOLTAGE = 12000;
 bool catapult_auto_shoot_enabled = false;
 
 // Sensors
-const float TRIBALL_LOADED_BRIGHTNESS = .0025;
+const float TRIBALL_LOADED_BRIGHTNESS = .01;
 const int CATAPULT_CHARGED_DEGREES = 3000;
 
 // PTO
@@ -26,13 +26,13 @@ bool wings_enabled = false;
 bool chassis_is_reversed = false;
 
 // Controls
-const ControlScheme t_controls(DIGITAL_R2, DIGITAL_L2, DIGITAL_R1, DIGITAL_L1, DIGITAL_Y, DIGITAL_DOWN, DIGITAL_B,
-                               DIGITAL_A, DIGITAL_X, TANK);
+const ControlScheme t_controls(DIGITAL_R1, DIGITAL_L1, DIGITAL_RIGHT, DIGITAL_LEFT, DIGITAL_B, DIGITAL_R2, DIGITAL_X,
+                               DIGITAL_A, DIGITAL_L2, TANK);
 const ControlScheme g_controls(DIGITAL_L1, DIGITAL_L2, DIGITAL_R1, DIGITAL_R2, DIGITAL_A, DIGITAL_B, DIGITAL_X,
-                               DIGITAL_Y, DIGITAL_DOWN, ARCADE);
+                               DIGITAL_Y, DIGITAL_DOWN, TANK);
 const ControlScheme k_controls(DIGITAL_L2, DIGITAL_L1, DIGITAL_R2, DIGITAL_R1, DIGITAL_B, DIGITAL_A, DIGITAL_Y,
                                DIGITAL_X, DIGITAL_DOWN, TANK);
-ControlScheme selected_controls = k_controls;
+ControlScheme selected_controls = g_controls;
 
 // Util
 const int MATCH_LENGTH = 105000; // = 105 seconds

6552f573486c1ab81eeccf1dc07cdffab0ee795c
K <Redknclarke@gmail.com>
2024-01-25 16:01:19 -0500

    updated ports

--- include/robot_config.hpp
+++ include/robot_config.hpp
@@ -3,7 +3,8 @@
 #include "main.h"
 
 // Define the chassis (PTO motors are last in the curly braces)
-Drive chassis({9, -10, -12, 11}, {-5, 4, 3, -2}, 7, 4.125, 600, 0.6);
+// Drive chassis({9, -10, -12, 11}, {-5, 4, 3, -2}, 7, 4.125, 600, 0.6);
+Drive chassis({9, -10, -21, 8}, {6, -4, 3, -5}, 2, 4.125, 600, 0.6);
 
 // Define Motors
 pros::Motor& PTO_intake = chassis.left_motors[3];
@@ -12,4 +13,4 @@ pros::Motor& PTO_flywheel = chassis.right_motors[3];
 // Define Pneumatics
 pros::ADIDigitalOut PTO_piston('B');
 pros::ADIDigitalOut wing_piston('C');
-pros::ADIDigitalOut lift_piston('A');
+pros::ADIDigitalOut lift_piston('A'); //!TEMP SWAPPED WITH PTO

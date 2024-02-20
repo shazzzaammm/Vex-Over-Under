b096aa5aed79f0a5e7b3144d881f09377e9dde95
K <Redknclarke@gmail.com>
2024-02-20 18:16:07 -0500

    updated drive and sensor ports

--- include/robot_config.hpp
+++ include/robot_config.hpp
@@ -4,7 +4,7 @@
 
 // Define the chassis
 // Left motors, Right motors, IMU, Wheel Diameter, RPM, Gear ratio
-Drive chassis({15, -8, -13, 14}, {-18, 17, 20, -19}, 16, 4.125, 600, 0.6);
+Drive chassis({15, -8, -13, 14}, {-18, 17, 20, -19}, 16, 4.125, 600, 0.6); 
 
 // Define Motors
 pros::Motor& PTO_intake = chassis.left_motors[3];
@@ -16,5 +16,5 @@ pros::ADIDigitalOut PTO_piston('B');
 pros::ADIDigitalOut hang_piston('C');
 
 // Define Sensors
-pros::Optical slapper_optic_sensor(5);
+pros::Optical slapper_optic_sensor(11);
 pros::Rotation slapper_rotation_sensor(12);
